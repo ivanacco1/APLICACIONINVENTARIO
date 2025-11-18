bd1f5cbb3e09bf59f5ac34e9c10d6028cd98ea02
Ivanacco1 <85845357+ivanacco1@users.noreply.github.com>
2025-11-18 19:23:42 -0300

    Modelos y base de datos

---
+++ APLICACIONINVENTARIO/Models/movimiento.cs
@@ -0,0 +1,14 @@
+﻿namespace APLICACIONINVENTARIO.Models
+{
+    public class Movimiento
+    {
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public string Tipo { get; set; } 
+        public int Cantidad { get; set; }
+        public string Motivo { get; set; }
+        public DateTime Fecha { get; set; }
+
+       
+        public Producto Producto { get; set; }
+    }
--- APLICACIONINVENTARIO/Models/producto.cs
+++ APLICACIONINVENTARIO/Models/producto.cs
@@ -1,6 +1,14 @@
 ﻿namespace APLICACIONINVENTARIO.Models
 {
-    public class producto
+    public class Producto
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }  
+        public int StockMinimo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool Activo { get; set; }
     }
 }
